4a8a56848e431f0f4867520c904d78e3b701e044
Ruslan Aliyev <ruslan@scaleflex.com>
2023-01-03 21:20:20 +0700

    attempt to add error-handling to image URL Cloudimage-ize | back to developing this

--- server/services/scaleflex-cloudimage.js
+++ server/services/scaleflex-cloudimage.js
@@ -139,38 +139,6 @@ module.exports = ({ strapi }) => ({
       },
     });
 
-    // await Promise.all(media.map(async (item, index) => {
-    //   let prepUrl = '';
-
-    //   if (/^https?:\/\//.test(item.url))
-    //   {
-    //     prepUrl = item.url.replace(/^https?:\/\//, '');
-    //   }
-    //   else
-    //   {
-    //     prepUrl = `${baseUrl}${item.url}`.replace(/^https?:\/\//, '');
-    //   }
-
-    //   let ciUrl = `https://${pluginConfig.domain}${pluginConfig.isV7 ? '/v7' : ''}/${prepUrl}`;
-
-    //   return await strapi.entityService.update('plugin::upload.file-delibrite-mistake', item.id, {
-    //     data: { 
-    //       url: ciUrl, 
-    //       formats: null 
-    //     },
-    //   }).catch(function(error) {
-    //     return JSON.stringify({error: error.message});
-    //   });
-    // }))
-    // .then(function(results) {
-    //   return JSON.stringify({success: true, results: results});
-    // })
-    // .catch(function(error) {
-    //   console.dir(error.message);
-
-    //   return JSON.stringify({success: false});
-    // });
-
     await Promise.all(media.map(async (item, index) => {
       let prepUrl = '';
 
@@ -185,13 +153,45 @@ module.exports = ({ strapi }) => ({
 
       let ciUrl = `https://${pluginConfig.domain}${pluginConfig.isV7 ? '/v7' : ''}/${prepUrl}`;
 
-      let updatedFileEntry = await strapi.entityService.update('plugin::upload.file', item.id, {
+      return await strapi.entityService.update('plugin::upload.file-delibrite-mistake', item.id, {
         data: { 
           url: ciUrl, 
           formats: null 
         },
+      }).catch(function(error) {
+        return JSON.stringify({error: error.message});
       });
-    }));
+    }))
+    .then(function(results) {
+      return JSON.stringify({success: true, results: results});
+    })
+    .catch(function(error) {
+      console.dir(error.message);
+
+      return JSON.stringify({success: false});
+    });
+
+    // await Promise.all(media.map(async (item, index) => {
+    //   let prepUrl = '';
+
+    //   if (/^https?:\/\//.test(item.url))
+    //   {
+    //     prepUrl = item.url.replace(/^https?:\/\//, '');
+    //   }
+    //   else
+    //   {
+    //     prepUrl = `${baseUrl}${item.url}`.replace(/^https?:\/\//, '');
+    //   }
+
+    //   let ciUrl = `https://${pluginConfig.domain}${pluginConfig.isV7 ? '/v7' : ''}/${prepUrl}`;
+
+    //   let updatedFileEntry = await strapi.entityService.update('plugin::upload.file', item.id, {
+    //     data: { 
+    //       url: ciUrl, 
+    //       formats: null 
+    //     },
+    //   });
+    // }));
 
     return media;
   },
